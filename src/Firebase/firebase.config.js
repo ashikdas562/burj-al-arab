969250829656a55bc8edc238a7b0c31909153e8d
Ashik Das <ashikdas562.com>
2022-06-02 02:58:10 +0600

    hello

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDR1NeJu-9MSfjeFgjoj4h9FeBLyIf23fM",
+    authDomain: "burj-al-arab-12039.firebaseapp.com",
+    projectId: "burj-al-arab-12039",
+    storageBucket: "burj-al-arab-12039.appspot.com",
+    messagingSenderId: "775796493776",
+    appId: "1:775796493776:web:da43564fda4dee93687dd5",
+    measurementId: "G-3YKS3GF9QN"
+  };
+
+
+  export default firebaseConfig;
---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+
+
+const initializeAuthentication = () => {
+  
+    initializeApp(firebaseConfig);
+
+}
+
+export default initializeAuthentication;
